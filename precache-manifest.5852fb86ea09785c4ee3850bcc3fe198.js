8bd9b6e4c80d9dd9f919ac5af496ea06b7111d80
Chayan Rai <chayanrai107@gmail.com>
2024-12-01 14:04:10 +0530

    Updates

---
+++ precache-manifest.5852fb86ea09785c4ee3850bcc3fe198.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e49436b8d6481c7acd88d11346afa93",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "8397320540b62f558727",
+    "url": "/WeatherApp/static/css/main.0c234360.chunk.css"
+  },
+  {
+    "revision": "2a253df2bf89ded834c9",
+    "url": "/WeatherApp/static/js/2.c9d5b9a0.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/WeatherApp/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "8397320540b62f558727",
+    "url": "/WeatherApp/static/js/main.9c6c8fd3.chunk.js"
+  },
+  {
+    "revision": "534b0356618092c05ab0",
+    "url": "/WeatherApp/static/js/runtime~main.b10a72ab.js"
+  },
+  {
+    "revision": "9950f8a65be1eb3360611a790edd910a",
+    "url": "/WeatherApp/static/media/clear_icon.9950f8a6.png"
+  },
+  {
+    "revision": "f96ea32908e21668e1755953245f610a",
+    "url": "/WeatherApp/static/media/cloud_icon.f96ea329.png"
+  },
+  {
+    "revision": "acc3b82a7dcac298f11738deb144510f",
+    "url": "/WeatherApp/static/media/rain_icon.acc3b82a.png"
+  },
+  {
+    "revision": "c6136500ad1dbb6c63fefa57c00f5806",
+    "url": "/WeatherApp/static/media/snow_icon.c6136500.png"
+  }
+]);
